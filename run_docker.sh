6c25ca25b93e177fecb97ad02212279f3e3071e0
Taiwo Amao <toamao08@outlook.com>
2023-01-06 07:58:23 +0000

    Fixes

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+## Complete the following steps to get Docker running locally.
 
 # Step 1:
 # Build image and add a descriptive tag
